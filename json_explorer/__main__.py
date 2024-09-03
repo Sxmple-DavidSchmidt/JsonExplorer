9b5b81a3c7a7d96457b8c6061cf34cfc47b5384f
david.schmidt <damidtyt@gmail.com>
2024-09-03 17:05:40 +0200

    add pyproject.toml

--- json_explorer/__main__.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-
-from importlib.metadata import entry_points
-from . import json_explorer
-
-def main(argv=None):
-    json_explorer.main(argv)
--- setup.py
+++ setup.py
@@ -1,11 +1,3 @@
 from setuptools import setup
 
-setup(
-    name='json_explorer',
-    version='0.0.1',
-    entry_points={
-        'console_scripts': [
-            'json_explorer=json_explorer.json_explorer:main'
-        ]
-    }
-)
+setup()
